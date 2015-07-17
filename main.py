9be40931508812b486b0fba7a09e0fb3dd198644
Luke Skibinski <l.skibinski@elifesciences.org>
2015-07-17 12:02:23 +0100

    got the basic tests I originally wrote working again. got a test runner script setup.

--- main.py
+++ main.py
@@ -1,17 +1,54 @@
 import os, sys
 import yaml
 
-descriptor_locations = [
-    "~/.ubr-backup.yaml",
-    "/etc/ubr/backup.yaml",
-]
+# utils
 
-def find_descriptors(location_list):
-    pass
+def first(lst):
+    try:
+        return lst[0]
+    except IndexError:
+        return None
+
+def rest(lst):
+    return lst[1:]
+
+def doall(val, *args):
+    "applies all of the functions given in args to the first val"
+    func = first(args)
+    if func:
+        return doall(func(val), *rest(args))
+    return val
+
+def list_paths(d):
+    return map(lambda f: os.path.join(d, f), os.listdir(d))
+
+# 
+
+def is_descriptor(path):
+    "returns true if the given path or filename looks like a backup descriptor"
+    fname = os.path.basename(path)
+    suffix = '-backup.yaml' # descriptors look like: elife-api-backup.yaml or lagotto-backup.yaml
+    return fname.endswith(suffix)
+
+def find_descriptors(descriptor_dir):
+    "returns a list of descriptors at the given path"
+    location_list = map(lambda path: doall(path, os.path.expanduser, os.path.abspath), list_paths(descriptor_dir))
+    return sorted(filter(is_descriptor, filter(os.path.exists, location_list)))
+
+
+def load_descriptor(descriptor):
+    import yaml
+    print 'given descriptor',descriptor
+    x = yaml.load(open(descriptor, "r"))
+    print 'loaded',x
+    return x
+
+#
+#
+#
 
 def main(args):
-    print args
-    pass
+    find_descriptors(args)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
--- tests/basic_tests.py
+++ tests/basic_tests.py
@@ -2,20 +2,64 @@ import os, unittest
 import main
 
 """These examples can be run with:
-   python -m unittest discover -s tests/unittests/ -p *_test.py
-
+   python -m unittest discover -s tests/ -p *_test.py
 
 """
 
+THIS_DIR = os.path.abspath(os.path.dirname(__name__))
+
 class BasicUsage(unittest.TestCase):
     def setUp(self):
-        pass
+        self.fixture_dir = os.path.join(THIS_DIR, "tests")
+        self.known_backup_fixtures = [
+            self.fixture("ubr-backup.yaml"),
+            self.fixture("ubr-2-backup.yaml"),
+        ]
 
     def tearDown(self):
         pass
 
-    def test_find_descriptors(self):
-        self.assertEqual(0, len(main.find_descriptors(".")))
+    def fixture(self, fname):
+         return os.path.join(self.fixture_dir, fname)
+
+    #
+    #
+    #
+
+    def test_find_no_descriptor(self):
+        "a directory with no descriptors in it returns an empty list"
+        self.assertEqual([], main.find_descriptors("/tmp/"))
+
+    def test_find_actual_description(self):
+        "a directory with descriptors in it returns each one ordered alphabetically"
+        found_descriptors = main.find_descriptors(self.fixture_dir)
+        for f in self.known_backup_fixtures:
+            # all known fixtures should be found
+            self.assertTrue(f in found_descriptors)
+        # just to be sure, assert both lists are the same size
+        self.assertEqual(len(set(found_descriptors)), len(self.known_backup_fixtures))
+
+    def test_find_bits(self):
+        fixture = self.known_backup_fixtures[0]
+        expected = {
+            'project-name': [
+                {'mysql': [
+                    'mydb1',
+                    'mydb2.table1',
+                ]},
+                
+                {'postgresql': [
+                    'dbx',
+                ]},
+                
+                {'files': [
+                    '/opt/thing/logs/',
+                    '/opt/thing/',
+                    '/opt/otherthing/reports/*.csv'
+                ]}
+            ]
+        }
+        self.assertEqual(expected, main.load_descriptor(fixture))
 
 if __name__ == '__main__':
     unittest.main()
