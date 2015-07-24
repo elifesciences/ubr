d4cad90d6c683b823e5240b954dcffa511826c52
Luke Skibinski <l.skibinski@elifesciences.org>
2015-07-24 16:41:52 +0100

    fixed typo

--- ubr.sh
+++ ubr.sh
@@ -3,7 +3,7 @@
 set -e
 source install.sh
 if [ -f /etc/ubr/config ]; then
-    source /etc/ubr
+    source /etc/ubr/config
 else
     echo "* couldn't find /etc/ubr/config - you had better hope all the defaults work"
 fi
