f293af98e60fcfdc9c4ad04b5a81be72f224a667
Luke Skibinski <l.skibinski@elifesciences.org>
2015-07-27 11:06:47 +0100

    shifted the mkdir up a wee bit

--- ubr.sh
+++ ubr.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 # calls the command line interface to the universal backup/restore script
+# assumes script is being run from directory it lives in
 set -e
 source install.sh
+
+# this script expects config to live in /etc/ubr/
+mkdir -p /etc/ubr/
 if [ -f /etc/ubr/config ]; then
     source /etc/ubr/config
 else
     echo "* couldn't find /etc/ubr/config - you had better hope all the defaults work"
 fi
-mkdir -p /etc/ubr/
+
+# call ubr
 python ubr/main.py /etc/ubr/
