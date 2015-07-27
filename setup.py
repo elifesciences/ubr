5b98fef45eae2fd668bc782f1831edc40bb5202b
Luke Skibinski <l.skibinski@elifesciences.org>
2015-07-27 10:26:50 +0100

    added setup script. added licensing information

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+MODULE = 'ubr' # name of the subdirectory your code resides in
+NAME = 'Universal Backup-Restore' # project name
+AUTHORS = ["Luke Skibinski <l.skibinski@elifesciences.org>"] # list of all contributing authors
+LICENCE = 'GPLv3' # licence short name
+COPYRIGHT = 'eLife Sciences' # copyright owner
+VERSION = '2015.5.27' # some sort of natural ordering key
+DESCRIPTION = 'A small library that reads backup "descriptors" in YAML format.'
+
+
+def groupby(f, l):
+    x, y = [], []
+    for v in l:
+        (x if f(v) else y).append(v)        
+    return x, y
+
+def requirements():
+    requisites = open('requirements.txt', 'r').read().splitlines()
+    pypi, non_pypi = groupby(lambda r: not r.startswith('-e '), requisites)
+    non_pypi = map(lambda v: v[len('-e '):], non_pypi)
+    return {
+        'install_requires': pypi,
+        'dependency_links': non_pypi,
+    }
+
+setup(
+    name = NAME,
+    version = VERSION,
+    description = DESCRIPTION,
+    long_description = open('README.md', 'r').read(),
+    packages = [MODULE],
+    license = open('LICENCE.txt', 'r').read(),
+    **requirements()
+)
