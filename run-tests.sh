66d5ec887ce9b2bc5001b074c6c8026fb41783c1
Luke Skibinski <l.skibinski@elifesciences.org>
2015-08-07 10:15:53 +0100

    added a switch to the scripts dir for runtests

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+cd "$(dirname "$0")"
 source install.sh 2&> /dev/null
 
 CONFIG=$1
@@ -14,4 +15,5 @@ else
 fi
 pylint2 -E *.py ubr/*.py ubr/tests/*.py
 echo 'passed pylint'
+#python2 -m unittest discover -s ubr/tests/ -p tgz_target_tests.py
 python2 -m unittest discover -s ubr/tests/ -p *_tests.py
