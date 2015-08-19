5b648735b4ad89f50169ff4d42283d517c4fca15
Luke Skibinski <l.skibinski@elifesciences.org>
2015-08-19 13:37:13 +0100

    fixed an edge case where there are a large number of files on the command line, causing the *kernel* to reject the system call with the ambiguous 127(mod 8) return code - which is not the same as /bin/sh's 127 return code

--- ubr/tgz_target.py
+++ ubr/tgz_target.py
@@ -34,19 +34,31 @@ def backup(path_list, destination):
     destination = os.path.join(destination, TMP_SUBDIR) # /tmp/foo/.tgz-tmp
 
     new_path_list = map(os.path.abspath, file_target.wrangle_files(path_list))
+
     utils.mkdir_p(destination)
     assert new_path_list, "files to backup are empty"
 
     ctd = destination # change to directory
-    targets = " ".join(new_path_list)
+    #targets = " ".join(new_path_list)
     filename = os.path.basename(original_destination) # this needs to change...!
     filename = 'archive'
     output_path = '%s/%s.tar.gz' % (original_destination, filename)
 
     # cd 2015-07-27--15-41-36/.tgz-tmp && tar cvzf 2015-07-27--15-41-36/archive.tar.gz *
     # cd /tmp/foo/.tgz-tmp && tar -P -cvzf /tmp/foo/archive.tar.gz *
-    cmd = 'cd %(ctd)s && tar cvzf %(output_path)s %(targets)s --absolute-names > /dev/null' % locals()
-    utils.system(cmd)
+    #cmd = "cd %(ctd)s && tar cvzf %(output_path)s %(targets)s --absolute-names" % locals()
+
+    # ok - why the manifest file? turns out there is only so many characters a shell allows,
+    # dictated by the kernal. in directories with lots of files, this length is exceeded
+    # quickly and returns a mysterious 32517 (or 127 mod 8) return code, which is documented
+    # as 'command not found', which is not the case at all.
+    manifest_path = '/tmp/ubr.manifest'
+    #new_path_list.append(manifest_path) # we don't want this file restored
+    open(manifest_path, 'w').write("\n".join(new_path_list))
+    
+    cmd = "cd %(ctd)s && tar cvzf %(output_path)s --files-from %(manifest_path)s --absolute-names" % locals()
+    logger.info("running command %r", cmd)
+    assert utils.system(cmd) == 0, "failed to create zip"
 
     return {
         'output': [output_path]
