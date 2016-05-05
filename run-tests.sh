64b66b20d89761fee629c34f9e09bcc5757befeb
Luke Skibinski <l.skibinski@elifesciences.org>
2016-05-05 17:39:24 +0100

    added nose test runner. shifted the unit tests into .unittest.sh (distinct from linting) and it now outputs code coverage. added an example.config file that is used for testing if an existing test-config file doesn't exist

--- run-tests.sh
+++ run-tests.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
 set -e
 cd "$(dirname "$0")"
-source install.sh 2&> /dev/null
-
-CONFIG=$1
-if [ ! -f "$CONFIG" ]; then
-    if [ -f /etc/ubr/config ]; then CONFIG="/etc/ubr/config"
-    elif [ -f "test-config" ]; then CONFIG="test-config"
-    fi
-fi
-
-if [ ! -z "$CONFIG" ]; then
-    echo "using config $CONFIG"
-    set -a; source $CONFIG; set +a;
-else
-    echo "no config file available for testing. looked for an argument, looked in /etc/ubr/config, looked for test-config and found *nothing*. I'll be using the system defaults! beware!"
-fi
-
+source install.sh
 source .lint.sh
-
-#python2 -m unittest discover -s ubr/tests/ -p s3_upload_tests.py 
-python2 -m unittest discover -s ubr/tests/ -p *_tests.py
+source .unittest.sh
