7232e3e0a0be6be1115bec2313917b6f251a9167
Luke Skibinski <l.skibinski@elifesciences.org>
2016-12-30 18:19:45 +0000

    shell and ubr scripts have the output from install.sh sent to /dev/null - too noisy to justify the odd case when we need it

--- shell.sh
+++ shell.sh
@@ -2,7 +2,7 @@
 # calls the command line interface to the universal backup/restore script
 # assumes script is being run from directory it lives in
 set -e
-. install.sh
+. install.sh > /dev/null
 
 # this script expects config to live in /etc/ubr/
 if [ -f /etc/ubr/config ]; then
