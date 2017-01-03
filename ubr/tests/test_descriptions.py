0190e76389ca65f6f2f8b92e9e0a77b53b433626
Luke Skibinski <l.skibinski@elifesciences.org>
2017-01-03 15:23:43 +0000

    shifted the tests from test_basics into test_descriptions and removed test_basics

--- ubr/tests/test_descriptions.py
+++ ubr/tests/test_descriptions.py
@@ -1,8 +1,33 @@
-import os
+import os, shutil
 from base import BaseCase
 from ubr import descriptions as descr
 
-class Descriptor(BaseCase):
+class FindDescriptors(BaseCase):
+    def setUp(self):
+        self.expected_output_dir = '/tmp/foo'
+        self.known_backup_fixtures = [
+            os.path.join(self.fixture_dir, "ubr-backup.yaml"),
+            os.path.join(self.fixture_dir, "ubr-2-backup.yaml"),
+        ]
+
+    def tearDown(self):
+        if os.path.exists(self.expected_output_dir):
+            shutil.rmtree(self.expected_output_dir)
+
+    def test_find_no_descriptor(self):
+        "a directory with no descriptors in it returns an empty list"
+        self.assertEqual([], descr.find_descriptors("/tmp/"))
+
+    def test_find_actual_description(self):
+        "a directory with descriptors in it returns each one ordered alphabetically"
+        found_descriptors = descr.find_descriptors(self.fixture_dir)
+        for f in self.known_backup_fixtures:
+            # all known fixtures should be found
+            self.assertTrue(f in found_descriptors)
+        # just to be sure, assert both lists are the same size
+        self.assertEqual(len(set(found_descriptors)), len(self.known_backup_fixtures))
+
+class LoadDescriptor(BaseCase):
     def setUp(self):
         self.known_backup_fixtures = [
             os.path.join(self.fixture_dir, "ubr-backup.yaml"),
@@ -11,6 +36,24 @@ class Descriptor(BaseCase):
 
     def tearDown(self):
         pass
+
+    def test_find_bits(self):
+        "what we load is what we expect"
+        fixture = self.known_backup_fixtures[0]
+        expected = {
+            'mysql-database': [
+                'mydb1',
+                'mydb2.table1',
+            ],
+
+            'files': [
+                '/opt/thing/logs/',
+                '/opt/thing/',
+                '/opt/otherthing/reports/*.csv',
+            ],
+
+        }
+        self.assertEqual(expected, descr.load_descriptor(fixture))
 
     def test_descriptor_correctness(self):
         descriptor = descr.load_descriptor(self.known_backup_fixtures[0])
