ee7da9f4fcfbf13552908536cce2bb4f01d49eed
Luke Skibinski <l.skibinski@elifesciences.org>
2017-03-14 13:35:49 +1030

    modified project_tests.sh to test with the credentials of the machine

--- project_tests.sh
+++ project_tests.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# new-style config
+cp /opt/ubr/app.cfg app.cfg
+
+# legacy config
 echo > test-config
 echo MYSQL_USER=elife-libraries >> test-config
 echo MYSQL_PWD=elife-libraries >> test-config
