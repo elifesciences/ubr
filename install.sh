d7f407e50c77511b5dd1ae26f109224a00d57a32
Luke <lsh-0@users.noreply.github.com>
2019-08-12 15:00:41 +0930

    switched to python 3.6

--- install.sh
+++ install.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
-# creates virtualenv, installs python dependencies
-# used by other scripts to ensure a working env exists
-set -e
+set -e # everything must succeed.
+echo "[-] install.sh"
 
-if [ ! -d venv ]; then
-    # build venv if one doesn't exist
-    virtualenv --python=`which python2` venv
-fi
+. mkvenv.sh
 
 source venv/bin/activate
 
-# link the default (elife) config if no app.cfg file found
 if [ ! -e app.cfg ]; then
     echo "* no app.cfg found! using the example settings (elife.cfg) by default."
     ln -s elife.cfg app.cfg
 fi
 
 pip install -r requirements.txt
+
+echo "[✓] install.sh"
