ddce4bd8608788a7b4a07c9bd4a02a44090f6641
Luke <lsh-0@users.noreply.github.com>
2019-08-14 17:57:33 +0930

    improved comment in scrub script

--- .scrub.sh
+++ .scrub.sh
@@ -5,6 +5,5 @@ if [ -e venv/bin/python3.6 ]; then
     pip install "black~=19.3b0"
     black ubr/ --target-version py34
 else
-    # seeing this on new 16.04 lax instances
-    echo "black requires Python 3.6+"
+    echo "black requires Python 3.6+, skipping code formatting."
 fi
