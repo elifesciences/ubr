a5fc2d18b807f9ec2a5afb07ec86e40d63281a14
Luke <lsh-0@users.noreply.github.com>
2019-08-14 12:32:00 +0930

    scrubbing only happens on python3.6

--- .scrub.sh
+++ .scrub.sh
@@ -2,6 +2,7 @@
 # automatic code formatting
 
 if [ -e venv/bin/python3.6 ]; then
+    pip install "black~=19.3b0"
     black ubr/ --target-version py34
 else
     # seeing this on new 16.04 lax instances
