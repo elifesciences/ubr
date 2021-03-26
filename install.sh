054eae4d337afaafd570ddad571b317eb43d51bd
Luke <lsh-0@users.noreply.github.com>
2021-03-26 11:42:07 +1030

    adds pipfile

--- install.sh
+++ install.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
-set -e # everything must succeed.
+set -e
 echo "[-] install.sh"
 
 . mkvenv.sh
 
 source venv/bin/activate
-
-if [ -e requirements.lock ]; then
-    # just delete the .lock file when you want to recreate it
-    pip install -r requirements.lock
-else
-    pip install -r requirements.txt
-    echo "locking..."
-    pip freeze > requirements.lock
-    echo "wrote 'requirements.lock'"
-fi
+pip install pip wheel --upgrade
+pip install -r requirements.txt
 
 if [ ! -e app.cfg ]; then
     echo "* no app.cfg found! using the example settings (elife.cfg) by default."
