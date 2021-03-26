d5c0a0bb1431c38006c36be094eeed3934b9aa2c
Luke <lsh-0@users.noreply.github.com>
2021-03-26 12:14:43 +1030

    pins pg8000 to 1.16.* as it doesn't use semver and 1.17 'has split the api in two'

--- ubr/descriptions.py
+++ ubr/descriptions.py
@@ -127,7 +127,7 @@ def validate_descriptor(descriptor):
 
 
 def load_descriptor(descriptor_path, path_list=[]):
-    descriptor = validate_descriptor(yaml.load(open(descriptor_path, "r")))
+    descriptor = validate_descriptor(yaml.safe_load(open(descriptor_path, "r")))
     if path_list:
         return subdescriptor(descriptor, path_list)
     return descriptor
