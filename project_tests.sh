25b738e63ffc17b828e56cd7e2a8a45fc4cbfd5e
Luke <lsh-0@users.noreply.github.com>
2022-04-26 15:59:44 +0930

    removed another mention of sourcing envvars

--- project_tests.sh
+++ project_tests.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 set -e
 
-# new-style config (see libraries formula)
+# test config (see libraries formula)
+# - https://github.com/elifesciences/elife-libraries-formula/blob/1f275e206f2b398ce49ba14e4a277905a5b8d332/salt/elife-libraries/init.sls#L132-L138
 rm -f app.cfg
 cp /etc/ubr-test-app.cfg app.cfg
-
-# legacy config
-echo > test-config
-echo MYSQL_USER=elife-libraries >> test-config
-echo MYSQL_PWD=elife-libraries >> test-config
 
 source test.sh
