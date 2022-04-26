e9f05a25a5a69e53401ecd0908903999dd7a309d
Luke <lsh-0@users.noreply.github.com>
2022-04-26 15:50:54 +0930

    conf, renamed 'elife.cfg' to 'example.cfg'

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-echo "[-] install.sh"
 
 . mkvenv.sh
 
@@ -9,8 +8,6 @@ pip install pip wheel --upgrade
 pip install -r requirements.txt --progress-bar off
 
 if [ ! -e app.cfg ]; then
-    echo "* no app.cfg found! using the example settings (elife.cfg) by default."
-    ln -s elife.cfg app.cfg
+    echo "* no app.cfg found! using the example settings (example.cfg) by default."
+    ln -sfT example.cfg app.cfg
 fi
-
-echo "[✓] install.sh"
