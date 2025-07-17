8e264f06d818d0ba0936b7cfc96f51e718d246c1
Scott Aubrey <scott@aubrey.org.uk>
2025-07-17 15:37:12 +0100

    make ubr.sh use mise as the runner

--- ubr.sh
+++ ubr.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # calls the command line interface to the universal backup/restore script
 # assumes script is being run from directory it lives in
+# usage: [-h] [--action [{config,check,check-all,backup,restore,download}]] [--location [{s3,file,rds-snapshot}]] [--hostname [HOSTNAME]] [--paths [PATHS [PATHS ...]]] [--no-progress-bar]
 set -e
-. install.sh > /dev/null
 
-# usage: ubr <backup|restore> <dir|s3> [target] [path]
-python -m ubr.main "$@"
+mise run --quiet ubr -- "$@"
