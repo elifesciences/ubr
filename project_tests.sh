a384dce9bef1415dbe5cd2ec08954c39a9599c70
Scott Aubrey <scott@aubrey.org.uk>
2025-07-17 09:03:12 +0100

    Remove projects tests that was only used for Jenkins elifesciences/issues#9321

--- project_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# test config (see libraries formula)
-# - https://github.com/elifesciences/elife-libraries-formula/blob/1f275e206f2b398ce49ba14e4a277905a5b8d332/salt/elife-libraries/init.sls#L132-L138
-rm -f app.cfg
-cp /etc/ubr-test-app.cfg app.cfg
-
-source test.sh
